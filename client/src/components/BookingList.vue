8fd4d6ba3562aee993e77eb1f809964e6af54310
Gordon Soutter <gordon.soutter@outlook.com>
2020-05-05 16:52:38 +0100

    adds app, bookings, list and booking item display. To be integrated

---
+++ client/src/components/BookingList.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+<div class="">
+  <h3>Bookings</h3>
+  <booking-item v-for="(booking, index) in bookings" :key="index" :booking="booking"></booking-item>
+
+</div>
+</template>
+
+<script>
+import BookingItem from '@/components/BookingItem.vue';
+
+
+export default {
+  name: 'booking-list',
+  props: ['bookings'],
+  components: {
+    'booking-item': BookingItem,
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
